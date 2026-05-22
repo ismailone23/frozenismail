335d2d5a18ad007f2c3919ccf07088e6f9e78eb0
ismailone23 <ismailhsanprsnl@gmail.com>
2026-05-22 15:04:37 +0600

    update | all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "DEV.PORTFOLIO",
+  title: "Ismail Hoosain",
   description: "Full-stack Developer & Designer",
 };
 
